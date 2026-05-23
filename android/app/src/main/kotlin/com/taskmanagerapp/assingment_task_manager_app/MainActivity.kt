86a12a788e17d1719b3c81a5296c6a5c368be685
Md Najim Islam <najimoffice21@gmail.com>
2026-05-23 11:56:58 +0600

    Create full Project Setup

---
+++ android/app/src/main/kotlin/com/taskmanagerapp/assingment_task_manager_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.taskmanagerapp.assingment_task_manager_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/task_list_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TaskListScreen extends StatelessWidget {
+  const TaskListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Task List Screen"),),
+    );
+  }
+}
